3e78fdd1122fcb8d2283af665346fa40f90d1c2b
wendlinger <karlw.maia@gmail.com>
2021-10-27 17:39:11 -0300

    First commit

---
+++ src/main/java/br/com/secretariat/SecretariatApplication.java
@@ -0,0 +1,13 @@
+package br.com.secretariat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretariatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretariatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/secretariat/SecretariatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.secretariat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretariatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
